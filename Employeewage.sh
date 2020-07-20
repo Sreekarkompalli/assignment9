f4aefe2752582a4c962ae27ec55d4a06ba134d00
sreekarkompalli <sreekarkompalli@gmail.com>
2020-07-20 12:06:05 +0530

    Employeewage

---
+++ Employeewage.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+Wage_per_Hour=20;
+Max_Hrs_In_Month=100
+Max_Working_Days_In_Month=20
+
+Emp_Working_Days=0;
+total_Emp_Hrs=0;
+
+result=1;
+isPresent=1;
+while [[ $EmpWorkingDays -ne $Max_Working_Days_In_Month || $EmpWorkingHours -le $Max_Hrs_In_Month 
+ ]]
+do
+echo $Days
+Emp_Working_Days=$((Emp_Working_Days+1))
+if [ $result -eq $isPresent ]
+then
+    echo "The employee is attented"
+    is_Part_time=1;
+    is_Full_time=2;
+    empCheck=$(($(($RANDOM%2))+1))
+    case $empCheck in
+    $is_Full_time)
+    empHrs=8
+    ;;
+    $is_Part_time)
+    empHrs=4
+    ;;
+*)
+    echo "Try to keep the number between one and two"
+    empHrs=0;
+    ;;
+esac
+fi
+total_Emp_Hrs=$(($total_Emp_Hrs+$empHrs))
+done
+totalSalary=$(($totalEmpHrs+$Wage_per_Hour))
+
+
+
+
